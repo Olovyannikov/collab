783710e57286124c2daff5fd596b4054f96a1648
Ilya Olovyannikov <excaliburgyro@gmail.com>
2024-10-14 22:45:21 +0300

    install deps

--- hono-entry.node.ts
+++ hono-entry.node.ts
@@ -1,21 +1,21 @@
-import { serve } from "@hono/node-server";
-import { serveStatic } from "@hono/node-server/serve-static";
-import { type Context, Hono } from "hono";
-import { env } from "hono/adapter";
-import { compress } from "hono/compress";
+import {serve} from "@hono/node-server";
+import {serveStatic} from "@hono/node-server/serve-static";
+import {type Context, Hono} from "hono";
+import {env} from "hono/adapter";
+import {compress} from "hono/compress";
 import app from "./hono-entry.js";
 
-const envs = env<{ NODE_ENV: string; PORT: string }>({ env: {} } as unknown as Context<object>);
+const envs = env<{ NODE_ENV: string; PORT: string }>({env: {}} as unknown as Context<object>);
 
 const nodeApp = new Hono();
 
 nodeApp.use(compress());
 
 nodeApp.use(
-  "/assets/*",
-  serveStatic({
-    root: `./dist/client/`,
-  }),
+    "/assets/*",
+    serveStatic({
+        root: `./dist/client/`,
+    }),
 );
 
 nodeApp.route("/", app!);
@@ -24,6 +24,6 @@ const port = envs.PORT ? parseInt(envs.PORT, 10) : 3000;
 
 console.log(`Server listening on http://localhost:${port}`);
 serve({
-  fetch: nodeApp.fetch,
-  port: port,
+    fetch: nodeApp.fetch,
+    port: port,
 });
